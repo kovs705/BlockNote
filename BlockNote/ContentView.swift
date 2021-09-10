54eb42e20d7b398cb3087519825bded0e45d05c8
kovs705 <smartkovsik@gmail.com>
2021-09-10 22:54:21 +0300

    some changes

--- BlockNote/ContentView.swift
+++ BlockNote/ContentView.swift
@@ -15,30 +15,40 @@ struct ContentView: View {
     @State private var noteName: String = ""
 
     var body: some View {
-        VStack {
-            
-            TextField("Note name...", text: $noteName, onCommit: {
-                addItem()
-            })
-            .padding()
-            
-            List {
-                ForEach(notes, id: \.self) { note in
-                    Text(note.name ?? "Unknown name")
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                #if os(iOS)
-                EditButton()
-                #endif
+        NavigationView {
+            VStack {
                 
-                Button(action: addItem) {
-                    Label("Add new Note!", systemImage: "plus")
+                TextField("Note name...", text: $noteName, onCommit: {
+                    addItem()
+                })
+                .padding()
+                .frame(width: UIScreen.main.bounds.width - 80, height: 55)
+                .contentShape(Rectangle())
+                .background(Color.white)
+                .font(.system(size: 18))
+                .cornerRadius(20)
+                
+                .lineLimit(1)
+                
+                List {
+                    ForEach(notes, id: \.self) { note in
+                        Text(note.name ?? "Unknown name")
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+                .toolbar {
+                    #if os(iOS)
+                    EditButton()
+                    #endif
+                    
+                    Button(action: addItem) {
+                        Label("Add new Note!", systemImage: "plus")
+                    }
                 }
             }
+            // end of the toolbar
         }
-        // end of the toolbar
+        // end of NavigationView
     }
 
     private func addItem() {
@@ -50,12 +60,12 @@ struct ContentView: View {
             newNote.noteID = (notes.last?.noteID ?? 0) + 1 // makes the order by id of the note
 
             do {
-                try viewContext.save()
+                try self.viewContext.save()
+                noteName = ""
             } catch {
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
-            self.noteName = ""
         }
     }
 
@@ -64,7 +74,7 @@ struct ContentView: View {
             offsets.map { notes[$0] }.forEach(viewContext.delete)
 
             do {
-                try viewContext.save()
+                try self.viewContext.save()
             } catch {
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
