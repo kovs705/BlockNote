70c576cac095e30bad3fcc4129ac91e27915623d
kovs705 <smartkovsik@gmail.com>
2021-10-01 00:42:44 +0300

    working on CoreData + GroupDetailVIew to show notes in the group

--- BlockNote/Concepts and views/GroupDetailView.swift
+++ BlockNote/Concepts and views/GroupDetailView.swift
@@ -6,14 +6,20 @@
 //
 
 import SwiftUI
+import CoreData
+
+    /// create a button to mark the note as completed
+    /// make a list of the nortes inside a group
+    /// make the button to delete the group with all the notes inside (ask a user to delete in or not)
 
 struct GroupDetailView: View {
-    
     let groupType: GroupType
+    let note: Note
     
     var body: some View {
         ScrollView(.vertical) {
             VStack {
+                // MARK: - TopBar Statistics
                 ZStack {
                     HStack {
                         Text("Number of words: ")
@@ -26,7 +32,7 @@ struct GroupDetailView: View {
                         Text("You've learned: ")
                             .bold()
                             .font(.system(size: 18))
-                        Text("Nothing yet!")
+                        Text("Nothing yet! Keep working on code, Eugene")
                         
                         Spacer()
                         Spacer()
@@ -36,16 +42,33 @@ struct GroupDetailView: View {
                 .cornerRadius(20)
                 .background(Color.offWhite)
                 .frame(width: UIScreen.main.bounds.width - 40, height: 70, alignment: .center)
+                
+                List {
+                    ForEach(groupType.typesArray, id: \.self) { note in
+                        
+                    }
+                }
+                
             }
+        }
+        .navigationTitle(groupType.wrappedName)
+    }
+}
+
+
+// /*
+struct GroupDetailView_Previews: PreviewProvider {
+    static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+ 
+    static var previews: some View {
+        let groupType = GroupType(context: moc)
+        let note = Note(context: moc)
+        groupType.name = "Test Group name"
+        groupType.color = "greenAvocado"
+        
+        return NavigationView {
+            GroupDetailView(groupType: groupType, note: note)
         }
     }
 }
-
-
-/*
-struct GroupDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        GroupDetailView(groupType:)
-    }
-}
- */
+//  */
