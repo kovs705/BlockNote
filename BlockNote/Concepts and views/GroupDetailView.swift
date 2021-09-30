9587cc179a87365571deaefd5e509759ab801adb
kovs705 <smartkovsik@gmail.com>
2021-10-01 00:57:41 +0300

    didn't check the GroupDetailView, but I hope it looks good and now I can go to sleep

--- BlockNote/Concepts and views/GroupDetailView.swift
+++ BlockNote/Concepts and views/GroupDetailView.swift
@@ -11,6 +11,8 @@ import CoreData
     /// create a button to mark the note as completed
     /// make a list of the nortes inside a group
     /// make the button to delete the group with all the notes inside (ask a user to delete in or not)
+    /// make an ability to put a red flag on notes that are important
+    /// --------------------------------
 
 struct GroupDetailView: View {
     let groupType: GroupType
@@ -40,14 +42,30 @@ struct GroupDetailView: View {
                 }
                 .shadow(color: .black.opacity(0.2), radius: 10, y: 5)
                 .cornerRadius(20)
-                .background(Color.offWhite)
+                .background(returnColorFromString(nameOfColor: groupType.color ?? "GreenAvocado"))
                 .frame(width: UIScreen.main.bounds.width - 40, height: 70, alignment: .center)
                 
                 List {
                     ForEach(groupType.typesArray, id: \.self) { note in
-                        
+                        HStack {
+                            Text("\(note.wrappedName)")
+                                .font(.system(size: 18))
+                                .bold()
+                                .foregroundColor(Color.textForeground)
+                            Spacer()
+                            VStack {
+                                // put a red flag here:
+                                
+                                Spacer()
+                            }
+                            Image(systemName: "chevron.forward")
+                                .foregroundColor(.gray)
+                                .font(.system(size: 18))
+                        }
+                        .frame(height: 40)
                     }
                 }
+                .padding(.horizontal)
                 
             }
         }
