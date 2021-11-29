f665baac03980e0a0c93456f654f85bf74814102
kovs705 <smartkovsik@gmail.com>
2021-11-29 23:50:46 +0300

    created view for cell

---
+++ BlockNote/Views/UIKit Views/Test/CellNoteView.swift
@@ -0,0 +1,23 @@
+//
+//  CellNoteView.swift
+//  BlockNote
+//
+//  Created by Kovs on 29.11.2021.
+//
+
+import SwiftUI
+
+struct CellNoteView: View {
+    
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CellNoteView_Previews: PreviewProvider {
+    static var previews: some View {
+        CellNoteView()
+    }
+}
