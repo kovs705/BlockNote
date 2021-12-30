1caf5bc48b2ecd8c47e42538b57768e417b8d366
kovs705 <smartkovsik@gmail.com>
2021-12-30 20:11:54 +0300

    working on UITableView

--- BlockNote/Views/UIKit Views/UINoteView.swift
+++ BlockNote/Views/UIKit Views/UINoteView.swift
@@ -8,9 +8,18 @@
 import Foundation
 import UIKit
 import SwiftUI
+import CoreData
 
 struct NoteList<Note, NoteItem, Row: View>: UIViewRepresentable {
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(notes, content)
+    }
+    
+    // MARK: - Make/Update
     func updateUIView(_ uiView: UITableView, context: Context) {
+        context.coordinator.data = notes
+        uiView.reloadData()
     }
     
     func makeUIView(context: Context) -> UITableView {
@@ -21,15 +30,36 @@ struct NoteList<Note, NoteItem, Row: View>: UIViewRepresentable {
         return tableView
     }
     
+    // MARK: - Properties
     private let content: (NoteItem) -> Row
-    private let notes: [Note]
-    private let noteItems: [NoteItem]
+    private let notes: [NoteItem]
     
-    init(_ notes: [Note], _ content: @escaping (NoteItem) -> Row, _ noteItems: [NoteItem]) {
+    // MARK: - init
+    init(_ notes: [NoteItem], _ content: @escaping (NoteItem) -> Row) {
         self.notes = notes
         self.content = content
-        self.noteItems = noteItems
     }
+    
+    // MARK: - Coordinator
+    class Coordinator: NSObject, UITableViewDataSource, UITableViewDelegate {
+        private let content: (NoteItem) -> Row
+        private let notes: [NoteItem]
+        
+        init(_ notes: [NoteItem], _ content: @escaping (NoteItem) -> Row) {
+            self.notes = notes
+            self.content = content
+        }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            notes.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            UITableViewCell()
+        }
+        
+    }
+    
     
     
 }
